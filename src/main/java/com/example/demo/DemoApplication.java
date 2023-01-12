fdac14c404fe7585b776430d32312b9baa10ef29
florence <florence590@163.com>
2023-01-12 19:08:50 +0800

    feat:redis cluster test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,10 @@
 
 package com.example.demo;
+import com.example.demo.util.RedisUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,13 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoApplication {
 
-
+	@Autowired
+	RedisUtil redisUtil;
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		redisUtil.set("hello","world");
+		System.out.println(redisUtil.get("hello"));
 		return String.format("Hello %s!", name);
 	}
 
