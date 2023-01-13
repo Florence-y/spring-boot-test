4f2841cad450cea71ff140c4a9840965036b9c0b
florence <florence590@163.com>
2023-01-13 23:05:05 +0800

    feat:redis cluster 拓扑刷新策略

--- src/main/java/com/example/demo/config/RedisConfig.java
+++ src/main/java/com/example/demo/config/RedisConfig.java
@@ -13,16 +13,12 @@ public class RedisConfig {
     @Bean
     public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
-
-        /* 设置value的序列化规则和 key的序列化规则 */
         StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
         GenericJackson2JsonRedisSerializer genericJackson2JsonRedisSerializer = new GenericJackson2JsonRedisSerializer();
-
-        redisTemplate.setKeySerializer(stringRedisSerializer);                // key采用String的序列化方式
-        redisTemplate.setHashKeySerializer(stringRedisSerializer);            // hash的key也采用String的序列化方式
-        redisTemplate.setValueSerializer(genericJackson2JsonRedisSerializer); // value序列化方式采用jackson
-        redisTemplate.setConnectionFactory(connectionFactory);                // 默认使用letttuce，如果想使用Jedis，创建JedisConnectionFactory实例作为参数传入
-
+        redisTemplate.setKeySerializer(stringRedisSerializer);
+        redisTemplate.setHashKeySerializer(stringRedisSerializer);
+        redisTemplate.setValueSerializer(genericJackson2JsonRedisSerializer);
+        redisTemplate.setConnectionFactory(connectionFactory);
         return redisTemplate;
     }
 }
--- src/main/java/com/example/demo/util/RedisUtil.java
+++ src/main/java/com/example/demo/util/RedisUtil.java
@@ -1,12 +1,12 @@
 package com.example.demo.util;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
+import javax.annotation.Resource;
 
 @Component
 public class RedisUtil {
-    @Autowired
+    @Resource
     private RedisTemplate<String, Object> redisTemplate;
 
     public Object get(String key) {
